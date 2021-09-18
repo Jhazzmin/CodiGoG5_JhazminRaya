bb6459494a217e33ab65360c5e24544c1fc4c13c
Jhazzmin <jhazmin239@gmail.com>
2021-09-18 18:40:56 -0500

    Update 01-javascript.js

--- Semana4/Dia3/01-javascript.js
+++ Semana4/Dia3/01-javascript.js
@@ -31,3 +31,35 @@ nombre ="Eduardo";
 
 console.log(nombre);
 
+//Cambiar de tipo de dato
+
+var anio = 2021;
+
+anio = "dos mil veintidos";
+
+console.log(anio);
+
+//JS es un lenguaje que es tipado, no pre declaramos los tipos de datos
+
+//VALORES RAROS EN VARIABLES
+
+var nota; //undefinied, no esta definido
+
+console.log(nota);
+
+var nulo = null; //deberia haber algo, pero no esta
+
+console.log(nulo);
+
+//Not a Number = NaN
+var rareza = 100 / "cebollas";
+
+console.log(rareza);
+
+var infinito = 100/0;
+
+
+console.log(infinito);
+
+
+console.log(typeof infinito);
