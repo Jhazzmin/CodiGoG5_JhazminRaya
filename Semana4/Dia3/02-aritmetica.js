c129a4e8f1a9addb19e51ea0688cbad9929295bf
Jhazzmin <jhazmin239@gmail.com>
2021-09-18 18:57:29 -0500

    Update 02-aritmetica.js

---
+++ Semana4/Dia3/02-aritmetica.js
@@ -0,0 +1,24 @@
+let z = 99;
+//let z = 1000000;
+console.log(z);
+
+//desde ahora utilicemos let
+
+let a= 100;
+
+let b= 4;
+
+let suma = a+b;
+console.log("Esto es una suma",suma);
+
+//puedo poner directamente codigo de JS dentro del console.log (para ver su resultado)
+console.log(1000+50);
+
+let resta = a-b;
+console.log("Resta", resta);
+
+let multiplicacion = a*b;
+console.log("Multiplicación",multiplicacion);
+
+let division = a/b;
+console.log("División", division);
