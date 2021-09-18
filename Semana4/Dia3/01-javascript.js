8a6ce74026eccd43abd67e195131b1dcd471875f
Jhazzmin <jhazmin239@gmail.com>
2021-09-18 18:23:53 -0500

    Update 01-javascript.js

---
+++ Semana4/Dia3/01-javascript.js
@@ -0,0 +1,33 @@
+//console.log permite mostraar en la consola de JS, la 
+//que se nos antoje es muy util para saber que esta
+//pasando internamente dentro de JS
+console.log("Hola Mundo!");
+
+//Variables
+//Palabras reservadas, JS las usa
+var nombre = "Jhazmin";
+console.log(nombre); //Solamente para mostrar algo
+
+//Tipos de datos - Texto, Numeros, Booleanos, 
+
+var nombre2 = "Silene";
+
+//Number - numero
+var edad = 31;
+
+console.log(edad);
+
+//Boolean - booleana
+var esDeNoche = true; //true o false
+console.log(esDeNoche);
+
+//para cambiar de valor a una variable, me basta con llamarla, esta variable tiene exisir antes
+
+edad = 25;
+
+console.log(edad);
+
+nombre ="Eduardo";
+
+console.log(nombre);
+
